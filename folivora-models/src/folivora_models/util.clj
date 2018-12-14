1e6e6d71979b9d388997dc00f52d6816c73cbbfd
namachan10777 <namachan10777@gmail.com>
2018-12-15 01:01:10 +0900

    improve util functions

--- folivora-models/src/folivora_models/util.clj
+++ folivora-models/src/folivora_models/util.clj
@@ -7,17 +7,19 @@
 (defn cube [size]
   (model/cube (nth size 0) (nth size 1) (nth size 2) :center false))
 
-(defn mul [rhs lhs]
-  [(* (nth rhs 0) (nth lhs 0))
-   (* (nth rhs 1) (nth lhs 1))
-   (* (nth rhs 2) (nth lhs 2))
-   ])
+(defn inv [arg]
+  (map - arg))
 
-(defn add [rhs lhs]
-  [(+ (nth rhs 0) (nth lhs 0))
-   (+ (nth rhs 1) (nth lhs 1))
-   (+ (nth rhs 2) (nth lhs 2))
-   ])
+(defn vecop [op args]
+  (defn f [v1 v2]
+    (map op v1 v2))
+  (reduce f args))
 
-(defn inv [opr]
-  [(- (nth opr 0)) (- (nth opr 1)) (- (nth opr 2))])
+(defn mul [arg & args]
+  (vecop * (cons arg args)))
+
+(defn add [arg & args]
+  (vecop + (cons arg args)))
+
+(defn sub [arg & args]
+  (vecop + (cons arg (map inv args))))
