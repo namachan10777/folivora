3195ff5f97b25d61076f60693651be7c0930cd8e
namachan10777 <namachan10777@gmail.com>
2019-01-14 02:48:39 +0900

    impl padding for key joint

--- folivora-models/src/folivora_models/key.clj
+++ folivora-models/src/folivora_models/key.clj
@@ -38,16 +38,36 @@
    (* -1 (nth key-size 2) (Math/sin tilt))
    (*  1 (nth key-size 2) (Math/cos tilt))])
 
+(defn col-joint-padding [tilt]
+  (let [w (nth key-size 0)
+        right [[0 0 0]
+               (key-pos-delta-joint 0)
+               (util/add (key-pos-delta-joint 0) (key-pos-delta-base tilt))]]
+    (model/polyhedron
+      (concat right (map (fn [p] (util/add [w 0 0] p)) right))
+      [[0 2 1]
+       [0 1 4 3]
+       [1 2 5 4]
+       [3 5 2 0]
+       [3 4 5]
+       ])))
+
 (defn key-col [near far]
   (defn make-far [times base-pos tilt]
     (let [current-key-elm
           (->> key-elm
+               (model/rotate tilt [1 0 0])
+               (model/translate base-pos))
+          current-key-padding
+          (->> (col-joint-padding key-elm-tilt)
+               (model/translate [0 (nth key-size 1) 0])
                (model/rotate tilt [1 0 0])
                (model/translate base-pos))]
       (if (<= times 1)
         current-key-elm
         (model/union
           current-key-elm
+          current-key-padding
           (make-far
             (- times 1)
             (util/add
@@ -57,11 +77,13 @@
               (key-pos-delta-base (+ tilt key-elm-tilt)))
             (+ tilt key-elm-tilt))))))
   (defn make-near [times]
-    (->> (make-far times
-                   (util/add
-                     (key-pos-delta-base key-elm-tilt)
-                     (key-pos-delta-joint 0))
-                   key-elm-tilt)
+    (->> (model/union
+           (make-far times
+                     (util/add
+                       (key-pos-delta-base key-elm-tilt)
+                       (key-pos-delta-joint 0))
+                     key-elm-tilt)
+           (col-joint-padding key-elm-tilt))
          (model/mirror [0 1 0])))
   (model/union
     (make-far (+ far 1) [0 0 0] 0)
