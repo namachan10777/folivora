888ec8d45e5ee50b052cd63d8c362d2d9a84f5d6
namachan10777 <namachan10777@gmail.com>
2019-01-28 01:31:03 +0900

    impl key_col

--- 3d/src/folivora.ml
+++ 3d/src/folivora.ml
@@ -3,4 +3,4 @@
 
 let () =
     let oc = open_out_gen [Open_wronly; Open_creat] 0o666 "folivora.scad" in
-    Scad.write oc Key.key_elm
+    Scad.write oc @@ Key.key_col 2 2
--- 3d/src/key.ml
+++ 3d/src/key.ml
@@ -22,4 +22,47 @@ module Key = struct
                 |> Model.translate
                     (center_move_vec key_size hole_size
                     |> Math.Pos.mul (1., 1., 0.))]
+
+    type sidewall_point_type =
+        | Top_far
+        | Bottom_far
+        | Joint
+        | Base
+
+    let get_x (x, _, _) = x
+    let get_y (_, y, _) = y
+    let get_z (_, _, z) = z
+
+    let sidewall_point tilt_step level spec =
+        let rec f tilt level spec = match level, spec with
+        | -1, _ -> 0., 0., 0.
+        | (_, Top_far) ->
+            Math.Pos.add (f tilt level Joint) (0., (cos tilt) *. (get_y key_size), (sin tilt) *. (get_y key_size))
+        | (_, Bottom_far) ->
+            Math.Pos.add (f tilt level Base) (0., (cos tilt) *. (get_y key_size), (sin tilt) *. (get_y key_size))
+        | (_, Base) ->
+            Math.Pos.add (f tilt level Joint) (0., (sin tilt) *. (get_z key_size), -. (cos tilt) *. (get_z key_size))
+        | (_, Joint) ->
+            f (tilt -. tilt_step) (level - 1) Top_far
+        in
+            if level >= 0
+            then Math.Pos.add (f (tilt_step *. float_of_int level) level spec) (0., 0., get_z key_size)
+            else
+                let spec = match spec with
+                | Top_far -> Joint
+                | Bottom_far -> Base
+                | Base -> Bottom_far
+                | Joint -> Top_far
+                in Math.Pos.add (f (tilt_step *. float_of_int (-level)) (-level) spec) (0., 0., get_z key_size)
+                |> Math.Pos.mul (1., -1., 1.)
+                |> Math.Pos.add (0., get_y key_size, 0.)
+        
+
+    let key_col near far =
+        Model.union @@
+            List.init (near + far + 1) @@ fun i ->
+                let i = i - near in
+                key_elm
+                |> Model.rotate (key_elm_tilt *. float_of_int i, 0., 0.)
+                |> Model.translate (sidewall_point key_elm_tilt i Base)
 end
