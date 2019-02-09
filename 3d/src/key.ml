c7f3a02473ee257b94bbdbac572e25f2acc74da2
namachan10777 <namachan10777@gmail.com>
2019-02-10 02:59:37 +0900

    3d: refactoring sidewall

--- 3d/src/key.ml
+++ 3d/src/key.ml
@@ -23,57 +23,38 @@ module Key = struct
                     (center_move_vec key_size hole_size
                     |> Math.Pos.mul (1., 1., 0.))]
 
-    type sidewall_point_type =
-        | Top_far
-        | Bottom_far
-        | Joint
-        | Base
-
     let get_x (x, _, _) = x
     let get_y (_, y, _) = y
     let get_z (_, _, z) = z
 
-    let sidewall_point tilt_step level spec =
-        let rec f tilt level spec = match level, spec with
-        | -1, _ -> 0., 0., 0.
-        | (_, Top_far) ->
-            Math.Pos.add (f tilt level Joint) (0., (cos tilt) *. (get_y key_size), (sin tilt) *. (get_y key_size))
-        | (_, Bottom_far) ->
-            Math.Pos.add (f tilt level Base) (0., (cos tilt) *. (get_y key_size), (sin tilt) *. (get_y key_size))
-        | (_, Base) ->
-            Math.Pos.add (f tilt level Joint) (0., (sin tilt) *. (get_z key_size), -. (cos tilt) *. (get_z key_size))
-        | (_, Joint) ->
-            f (tilt -. tilt_step) (level - 1) Top_far
-        in
-            Math.Pos.add (f (tilt_step *. float_of_int level) level spec) (0., 0., get_z key_size)
+    let key_joint_pos_series tilts =
+        get_x @@ List.fold_left
+            (fun (ps, p, tilt_acc) tilt ->
+                let f g = (get_y key_size) *. (g tilt_acc) in
+                    let p = Math.Pos.add p (0., f cos, f sin) in
+                    (p :: ps, p, (tilt_acc +. tilt)))
+            ([], (0., 0., 0.), 0.) tilts
 
-        
+    let tilt_acc tilts = List.fold_left (fun (tilts, acc) tilt ->
+        let acc = acc +. tilt in (acc :: tilts), acc) ([], 0.) tilts |> fst
 
-    let key_elm_joint_padding_with_pos tilt i =
-        let bottom1 = sidewall_point tilt i Bottom_far in
-        let joint   = sidewall_point tilt i Top_far in
-        let bottom2 = sidewall_point tilt (i+1) Base in
-        let width = get_x key_size in
-        let bottom_surface = [bottom1; joint; bottom2] in
-        Model.polyhedron
-            (bottom_surface @ List.map (Math.Pos.add (width, 0., 0.)) bottom_surface)
-            [[0; 2; 1]; [0; 1; 4; 3]; [3; 4; 5]; [0; 3; 5; 2]; [1; 2; 5; 4]]
+    let key_base_pos_series tilts =
+        List.map2 (fun jp tilt ->
+            Math.Pos.add jp (0., (get_z key_size) *. (sin tilt), -.(get_z key_size) *. (cos tilt)))
+        (key_joint_pos_series tilts) (tilt_acc tilts)
+
+    let key_tip_pos_series tilts =
+        List.map2 (fun bp tilt ->
+            Math.Pos.add bp (0., (get_y key_size) *. (cos tilt), (get_y key_size) *. (sin tilt)))
+        (key_base_pos_series tilts) (tilt_acc tilts)
 
     let key_col near far =
-        let make_range start finish =
-            Model.union @@
-                (List.init (finish-start) @@ fun i ->
-                    let i = i + start in
-                    key_elm
-                    |> Model.rotate (key_elm_tilt *. float_of_int i, 0., 0.)
-                    |> Model.translate (sidewall_point key_elm_tilt i Base))
-                @ (List.init (finish-start-1) @@ fun i ->
-                    let i = i + start in
-                    key_elm_joint_padding_with_pos key_elm_tilt i) in
-        Model.union [
-            make_range 0 (far+1);
-            (Model.union [make_range 1 (near+1); key_elm_joint_padding_with_pos key_elm_tilt 0])
-            |> Model.mirror (0, 1, 0)
-            |> Model.translate (0., get_y key_size, 0.);
-        ]
+        let make_col tilts =
+            List.map2 (fun bp tilt ->
+                key_elm
+                |> Model.rotate (tilt, 0., 0.)
+                |> Model.translate (0., 0., get_z key_size)
+                |> Model.translate bp)
+            (key_base_pos_series tilts) (tilt_acc tilts)
+        in Model.union (key_elm :: (make_col near))
 end
