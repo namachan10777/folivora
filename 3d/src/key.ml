f5ca6d4c15d812be4f67dbab86448754b0b5875b
namachan10777 <namachan10777@gmail.com>
2019-02-10 14:45:45 +0900

    3d: fix key_joint_pos

--- 3d/src/key.ml
+++ 3d/src/key.ml
@@ -28,20 +28,25 @@ module Key = struct
     let get_z (_, _, z) = z
 
     let key_joint_pos_series tilts =
-        get_x @@ List.fold_left
-            (fun (ps, p, tilt_acc) tilt ->
-                let f g = (get_y key_size) *. (g tilt_acc) in
-                    let p = Math.Pos.add p (0., f cos, f sin) in
-                    (p :: ps, p, (tilt_acc +. tilt)))
-            ([], (0., 0., 0.), 0.) tilts
+        fst @@ List.fold_left
+            (fun (ps, tilt_acc) tilt ->
+                let f g = (get_y key_size) *. (g (tilt_acc +. tilt)) in
+                    let p = Math.Pos.add (List.hd ps) (0., f cos, f sin) in
+                    (p :: ps, (tilt_acc +. tilt)))
+            ([(0., 0., 0.)], 0.) tilts
 
     let tilt_acc tilts = List.fold_left (fun (tilts, acc) tilt ->
         let acc = acc +. tilt in (acc :: tilts), acc) ([], 0.) tilts |> fst
 
+    let rec drop = function
+        | [] -> raise (Invalid_argument "drop")
+        | [t] -> []
+        | h :: t -> h :: drop t
+
     let key_base_pos_series tilts =
         List.map2 (fun jp tilt ->
             Math.Pos.add jp (0., (get_z key_size) *. (sin tilt), -.(get_z key_size) *. (cos tilt)))
-        (key_joint_pos_series tilts) (tilt_acc tilts)
+        (key_joint_pos_series tilts |> List.tl) (tilt_acc tilts)
 
     let key_tip_pos_series tilts =
         List.map2 (fun bp tilt ->
@@ -56,26 +61,27 @@ module Key = struct
         | ([], [], []) -> []
         | (e1::l1, e2::l2, e3::l3) -> (f e1 e2 e3) :: map3 f l1 l2 l3
         | _ -> raise (Invalid_argument "map3")
+
  
     let key_col near far =
         let make_col tilts =
             List.map2 (fun bp tilt ->
                 key_elm
                 |> Model.rotate (tilt, 0., 0.)
-                |> Model.translate (0., 0., get_z key_size)
+                |> Model.translate (0., get_y key_size, get_z key_size)
                 |> Model.translate bp)
             (key_base_pos_series tilts) (tilt_acc tilts) in
         let make_paddings tilts =
             let ps = map3
                 (fun b j t -> t :: j :: b :: [])
                 (key_base_pos_series tilts)
-                (key_joint_pos_series tilts)
+                (key_joint_pos_series tilts |> List.tl)
                 (key_tip_pos_series tilts) |> List.flatten in
             let rec f = function
                 | b :: j :: t :: tl ->
                     (key_padding [b; j; t]) :: f tl
                 | _ -> []
-            in f ((0., get_y key_size, -.(get_z key_size)) :: (List.rev ps))
-                |> List.map (Model.translate (0., 0., get_z key_size))
+            in f ((0., 0., -.(get_z key_size)) :: (List.rev ps))
+                |> List.map (Model.translate (0., get_y key_size, get_z key_size))
         in Model.union (key_elm :: (make_col near) @ (make_paddings near))
 end
