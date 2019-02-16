cd96d16c30041beee85e6270eaf954c599e3dc78
namachan10777 <namachan10777@gmail.com>
2019-02-17 03:44:33 +0900

    fix block thickness

--- 3d/src/block.ml
+++ 3d/src/block.ml
@@ -14,7 +14,7 @@ let ( ** ) = pow
 
 module Block = struct
     let eps = 0.5
-    let block_thick = 5.0
+    let block_thick = 5.08
     let slit_width = 2.54 *. 5.0 +. eps
     let block_width = 19.0
     let slit_thick = 1.8 +. eps
