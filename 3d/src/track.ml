8a2d1b4e57b0cb56fba3906b1afdfd35ea2b3d65
namachan10777 <namachan10777@gmail.com>
2019-02-20 23:35:50 +0900

    refactor track.ml

--- 3d/src/track.ml
+++ 3d/src/track.ml
@@ -1,55 +1,60 @@
 #use "./scad_ml/src/scad.ml";;
 
 module Track = struct
-    let pole_d = 41.9
+    let pillar_d = 41.9
+
+
     let ball_r = 17.0
-    let ball_clearance = 0.5
-    let hole_d = 1.7
-    let hole_clearance = 3.
+    let ball_c = 0.5
+
+    let hole_r = 1.7
+    let hole_c = 3.
+
     let bearing_shaft_r = 1.6
     let bearing_shaft_h = 8.
-    let bearing_r = 3.5
-    let bearing_clearance = 0.4
-    let bearing_thick = 4.
 
-    let bearing_hole =
+    let bearing_r = 3.5
+    let bearing_c = 0.4
+    let bearing_t = 4.
+
+    let bearing_hollowing =
         Model.union [
             Model.cylinder bearing_shaft_r bearing_shaft_h ~fn:30
-            |> Model.rotate (0., pi /. 2., 0.);
+                |> Model.rotate (0., pi /. 2., 0.);
             Model.cube (bearing_shaft_r, bearing_shaft_r*.2.,  bearing_shaft_h)
-            |> Model.rotate (0., pi /. 2., 0.)
-            |> Model.translate (0., -.bearing_shaft_r, bearing_shaft_r);
-            Model.cylinder (bearing_r+.bearing_clearance) bearing_thick ~fn:30
-            |> Model.rotate (0., pi /. 2., 0.)
-            |> Model.translate ((bearing_shaft_h -. bearing_thick) /. 2., 0., 0.)
+                |> Model.rotate (0., pi /. 2., 0.)
+                |> Model.translate (0., -.bearing_shaft_r, bearing_shaft_r);
+            Model.cylinder (bearing_r+.bearing_c) bearing_t ~fn:30
+                |> Model.rotate (0., pi /. 2., 0.)
+                |> Model.translate ((bearing_shaft_h -. bearing_t) /. 2., 0., 0.)
         ]
 
-    let bearing_holes r =
-        let hole = bearing_hole
+    let bearinghedge r =
+        let hole = bearing_hollowing
             |> Model.translate (-.bearing_shaft_h/.2., r, 0.) in
         Model.union [
             hole;
-            hole |> Model.rotate (0., 0., pi *. 2. /. 3.);
+            hole |> Model.rotate (0., 0.,    pi *. 2. /. 3.);
             hole |> Model.rotate (0., 0., -. pi *. 2. /. 3.)
         ]
 
-    let under_block h =
+    let foundation h =
         let t = ball_r -. h in
         let bearings_r = sqrt ((ball_r +. bearing_r) ** 2 -. h ** 2) in
         Model.difference
             (Model.union [
-                Model.cube (pole_d +. hole_clearance *. 2., pole_d, t);
+                Model.cube (pillar_d +. hole_c *. 2., pillar_d, t);
             ])
             [
-                (Model.translate (pole_d /. 2. +. 3., hole_clearance, 0.)
-                    (Model.cylinder hole_d t ~fn:30));
-                (Model.translate (3., pole_d /. 2. +. 3., 0.)
-                    (Model.cylinder hole_d t ~fn:30));
-                (Model.translate (pole_d +. 3., pole_d /. 2. +. 3., 0.)
-                    (Model.cylinder hole_d t ~fn:30));
-                bearing_holes bearings_r
-                |> Model.translate (pole_d /. 2. +. 3., pole_d /. 2., t -. bearing_shaft_r);
-                Model.sphere (ball_r +. ball_clearance) ~fn:30
-                |> Model.translate (pole_d /. 2. +. 3., pole_d /. 2., t -. bearing_shaft_r +. h)
+                Model.cylinder hole_r t ~fn:30
+                    |> Model.translate (pillar_d /. 2. +. hole_c, hole_c, 0.);
+                Model.cylinder hole_r t ~fn:30
+                    |> Model.translate (hole_c, pillar_d /. 2. +. hole_c, 0.);
+                Model.cylinder hole_r t ~fn:30
+                    |> Model.translate (pillar_d +. hole_c, pillar_d /. 2. +. hole_c, 0.);
+                bearinghedge bearings_r
+                    |> Model.translate (pillar_d /. 2. +. hole_c, pillar_d /. 2., t -. bearing_shaft_r);
+                Model.sphere (ball_r +. ball_c) ~fn:50
+                    |> Model.translate (pillar_d /. 2. +. hole_c, pillar_d /. 2., t -. bearing_shaft_r +. h)
             ]
 end
