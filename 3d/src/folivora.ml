f5bfac16adb3c3701b035128c9e44578754e967b
namachan10777 <namachan10777@gmail.com>
2019-03-19 15:35:42 +0900

    delete 3d/src/folivora.ml

--- 3d/src/folivora.ml
+++
@@ -1,8 +0,0 @@
-#use "./scad_ml/src/scad.ml"
-#use "./src/track.ml"
-
-let alpha = pi/.12.
-
-let () =
-    let oc = open_out_gen [Open_wronly; Open_creat] 0o666 "folivora.scad" in
-    Scad.write oc @@ Track.mold (pi /. 10.) 7.0
