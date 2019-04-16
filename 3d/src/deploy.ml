24fcd0600284f34f630d0a88cd74d736b6274b1e
namachan10777 <namachan10777@gmail.com>
2019-04-17 00:12:16 +0900

    refactor key.ml

--- 3d/src/deploy.ml
+++ 3d/src/deploy.ml
@@ -1,6 +1,5 @@
 #use "./src/common.ml"
 #use "./src/key.ml"
-#use "./src/thumb.ml"
 
 let angle_unit = pi /. 18.
 
@@ -10,15 +9,4 @@ let build scad filename =
     close_out oc
 
 let () =
-    build (Thumb.nut_insert) "nutinsert.scad";
-    build (Thumb.thumb_keys) "thumbkey.scad";
-    build (Thumb.thumb_track) "thumbtrack.scad";
-    build (Thumb.trackball_cover) "trackball_cover.scad";
-    build (Key.key_main [
-        (2, 0., 0.);
-        (2, 2., -.1.);
-        (2, 5., -.2.);
-        (2, 3., -.1.);
-        (1, -3., -.1.);
-        (1, -5., -.2.);
-    ]) "key.scad"
+    build (Key.kailh_lp (21., 18., 5.)) "key.scad"
