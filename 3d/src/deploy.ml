7f593c59bb7cd8560187242ec50242c69afd0bc3
namachan10777 <namachan10777@gmail.com>
2019-04-18 17:20:05 +0900

    fix compile script

--- 3d/src/deploy.ml
+++ 3d/src/deploy.ml
@@ -5,7 +5,7 @@
 let angle_unit = pi /. 18.
 
 let build scad filename = 
-    let oc = open_out_gen [Open_wronly; Open_creat] 0o666 filename in
+    let oc = open_out_gen [Open_wronly; Open_trunc; Open_creat] 0o666 filename in
     Scad.write oc scad;
     close_out oc
 
