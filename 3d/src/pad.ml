9777a230f378c0959b6ebc0ebfff4a156b2f1aff
namachan10777 <namachan10777@gmail.com>
2019-04-18 18:10:37 +0900

    expand params to near side

--- 3d/src/pad.ml
+++ 3d/src/pad.ml
@@ -6,7 +6,7 @@ module type PadConf = sig
     val len_wall_clearance: float
     val gen_len_wall: bool
     val col_d: float
-    val params: (int * float * float) list
+    val params: (int * int * float * float) list
 end
 
 module Pad (C: PadConf) = struct
@@ -18,22 +18,25 @@ module Pad (C: PadConf) = struct
     let block = C.keygen C.block_size |>> (w/.2., d/.2., 0.)
     let o = (0., 0., 0.)
 
-    let block_local_p bending n = List.init n (fun i -> C.far_curve *. float_of_int i)
+    let block_local_p bending n = List.init n (fun i -> bending *. float_of_int (i+1))
         |> List.fold_left (fun p bending -> p <+> (0., d *. cos bending, d *.sin bending)) (0., 0., 0.)
 
-    let mov_block_local bending n scad = scad |>> block_local_p bending n
+    let mov_block_local bending n scad = scad |@> (bending *. float_of_int (n+1), 0., 0.) |>> block_local_p bending n
 
     (* 0 <= x < List.length params *)
     let block_p x y =
         let xp = (C.col_d +. w) *. float_of_int x in
-        let (n, dy, dz) = List.nth C.params x in
+        let (n, m, dy, dz) = List.nth C.params x in
         if y = 0
         then (xp, dy, dz)
         else if y > 0
-        then (xp, dy +. d, dz) <+> block_local_p C.far_curve n
-        else (xp, dy, dz) <+> ((block_local_p C.far_curve 1) <*> (1., -1., 1.))
+        then (xp, dy +. d, dz) <+> block_local_p C.far_curve y
+        else (xp, dy, dz) <+> ((block_local_p C.far_curve (-y)) <*> (1., -1., 1.))
 
-    let mov_block x y scad = scad |>> block_p x y
+    let mov_block x y scad =
+        if y > 0
+        then scad |@> (C.far_curve *. float_of_int y, 0., 0.) |>> block_p x y
+        else scad |>> (0., -.d, 0.) |@> (C.near_curve *. float_of_int y, 0., 0.) |>> block_p x y
 
     let gen_col_half bending n = M.union @@ List.init n (fun i -> mov_block_local bending i block)
     let gen_bond_half (n, n') (dy, dz) bending =
@@ -60,18 +63,18 @@ module Pad (C: PadConf) = struct
 
     let pad params =
         let rec build x = function
-            | (n, dy, dz) :: ((n', dy', dz') as succ) :: tl ->
+            | (n, m, dy, dz) :: ((n', m', dy', dz') as succ) :: tl ->
                 let col_far = gen_col_half C.far_curve n |>> (0., d, 0.) in
                 let bond_far = gen_bond_half (n, n') (dy'-.dy, dz'-.dz) C.far_curve |>> (w, d, 0.) in
-                let col_near = gen_col_half C.near_curve 1 |> M.mirror (0, 1, 0) in
-                let bond_near = gen_bond_half (1, 1) (dy-.dy', dz'-.dz) C.near_curve |> M.mirror (0, 1, 0) |>> (w, 0., 0.) in
+                let col_near = gen_col_half C.near_curve m |> M.mirror (0, 1, 0) in
+                let bond_near = gen_bond_half (m, m') (dy-.dy', dz'-.dz) C.near_curve |> M.mirror (0, 1, 0) |>> (w, 0., 0.) in
                 let center = block in
                 let bond_center = M.hull [M.cube (eps, d, h); M.cube (eps, d, h) |>> (C.col_d, dy'-.dy, dz'-.dz)] |>> (w, 0., 0.) in
                 (M.union [col_far; bond_far; col_near; bond_near; center; bond_center] |>> (x, dy, dz))
                 :: build (x +. w +. C.col_d) (succ :: tl)
-            | (n, dy, dz) :: [] ->
+            | (n, m, dy, dz) :: [] ->
                 let col_far = gen_col_half C.far_curve n |>> (0., d, 0.)in
-                let col_near = gen_col_half C.near_curve 1 |> M.mirror (0, 1, 0) in
+                let col_near = gen_col_half C.near_curve m |> M.mirror (0, 1, 0) in
                 let center = block in
                 (M.union [col_far; col_near; center] |>> (x, dy, dz)) :: []
             | [] -> []
