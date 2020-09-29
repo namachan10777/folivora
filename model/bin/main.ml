140ee18ee8d3ab80a49b29436ddf34fd8c512d93
Nakano Masaki <namachan10777@gmail.com>
2020-09-30 01:14:29 +0900

    gen_center_with_bridge

--- model/bin/main.ml
+++ model/bin/main.ml
@@ -8,35 +8,39 @@ let key_size = (19.0, 19.0, 5.0)
 let cherry_mx = Key_unit.cherry_mx
 let pi = Scad_ml.Core.pi
 
+let pad_conf = {
+    Pad.key_t = 5.0;
+    cols = [
+        {
+            Pad.w = 16.0;
+            Pad.near = [];
+            Pad.far = [];
+            Pad.diff = (0.0, 0.0, 0.0);
+            Pad.center = (16.0, cherry_mx)
+        };
+        {
+            Pad.w = 16.0;
+            Pad.near = [];
+            Pad.far = [];
+            Pad.diff = (2.0, 2.0, -1.0);
+            Pad.center = (16.0, cherry_mx)
+        };
+        {
+            Pad.w = 16.0;
+            Pad.near = [];
+            Pad.far = [];
+            Pad.diff = (2.0, 2.0, -2.0);
+            Pad.center = (16.0, cherry_mx)
+        };
+        {
+            Pad.w = 16.0;
+            Pad.near = [];
+            Pad.far = [];
+            Pad.diff = (2.0, -2.0, 2.0);
+            Pad.center = (16.0, cherry_mx)
+        };
+    ];
+}
+
 let () =
-	let (model, _) = Thumb.f [
-		(key_size, cherry_mx, pi /. 12.);
-		(key_size, cherry_mx, pi /. 6.);
-		(key_size, cherry_mx, pi /. 12.);
-	]
-	((19.0, 19.0, 5.0), Key_unit.cherry_mx)
-	[
-		(key_size, cherry_mx, pi /. 12.);
-		(key_size, cherry_mx, pi /. 6.);
-		(key_size, cherry_mx, pi /. 12.);
-	]
-	in
-	let pad = Pad.f [
-		{
-			Pad.d = (0.0, 0.0, 0.0);
-			Pad.base = (key_size, cherry_mx);
-			Pad.far = [
-				(key_size, cherry_mx, pi /. 6.);
-				(key_size, cherry_mx, pi /. 12.);
-				(key_size, cherry_mx, pi /. 24.);
-				(key_size, cherry_mx, pi /. 12.);
-				(key_size, cherry_mx, pi /. 6.);
-			];
-			Pad.near = [
-				(key_size, cherry_mx, pi /. 12.);
-			];
-		};
-	]
-	in
-	build pad "pad.scad";
-	build model "thumb.scad";
+    build (Pad.f pad_conf) "pad.scad"
--- model/bin/pad.ml
+++ model/bin/pad.ml
@@ -1,43 +1,35 @@
 module M = Scad_ml.Model
 open Scad_ml.Util
 
-type size_t = float * float * float
-type column_t = (size_t * (size_t -> Scad_ml.Core.scad_t) * float) list
-
-type col = {
-	d: size_t;
-	base: size_t * (size_t -> Scad_ml.Core.scad_t);
-	far: column_t;
-	near: column_t;
+type col_t = {
+    w : float;
+    diff : Scad_ml.Math.t;
+    near : (float * float * (Scad_ml.Math.t -> Scad_ml.Core.scad_t)) list;
+    far : (float * float * (Scad_ml.Math.t -> Scad_ml.Core.scad_t)) list;
+    center : float * (Scad_ml.Math.t -> Scad_ml.Core.scad_t);
 }
 
-let gen_col c =
-	let center_size = fst c.base in
-	let (_, d, h) = center_size in
-	let center = (snd c.base) center_size |>> (0., 0., -.h) in
-	let rec gen trans p angle_acc = function
-		| [] -> []
-		| key :: remain ->
-			let (key_size, key_gen, angle) = key in
-			let (w, d, h) = key_size in
-			let (x, y, z) = p in
-			let angle_acc' = angle_acc +. angle in
-			let plate = M.cube (w, 0.0001, h) |>> (0., 0., -.h) in
-			let joint = M.hull [
-				plate |@> (angle_acc, 0.0, 0.0) |>> p;
-				plate |@> (angle_acc+.angle, 0.0, 0.0) |>> p;
-			] in
-			let key = trans key_gen key_size  |>> (0., 0., -.h) |@> (angle_acc', 0.0, 0.0) |>> p in
-			key :: joint :: gen trans (x, y +. d *. cos angle_acc', z +. d *. sin angle_acc') angle_acc' remain
-	in
-	let far = gen (fun gen size -> gen size) (0.0, d, 0.0) 0.0 c.far in
-	let near =
-		gen (fun gen size -> gen size |> M.mirror (0, 1, 0) |>> (0., get_y size, 0.)) (0.0, 0.0, 0.0) 0.0 c.near
-		|> M.union |> M.mirror (0, 1, 0)
-	in
-	M.union (center :: near :: far)
+type pad_conf_t = {
+    key_t : float;
+    cols : col_t list;
+}
 
-let f = function
-	| [] -> M.union []
-	| c :: []  -> gen_col c
-	| c1 :: _ :: _ -> gen_col c1
+let gen_base_with_joint t =
+    let rec f p_acc = function
+    | [] -> []
+    | (w, d, diff, gen) :: [] -> [gen (w, d, t) |>> (diff <+> p_acc)]
+    | (w, d, diff, gen) :: ((_, d', diff', _) :: _ as remain) ->
+        (gen (w, d, t) |>> (diff <+> p_acc))
+        :: (M.hull [
+            M.cube (0.001, d, t);
+            M.cube (0.001, d', t) |>> diff';
+        ] |>> (p_acc <+> (w, 0., 0.) <+> diff))
+        :: f (p_acc <+> diff <+> (w, 0., 0.)) remain
+    in
+    f (0., 0., 0.)
+
+let f conf =
+    conf.cols
+    |> List.map (fun col -> (col.w, fst col.center, col.diff, snd col.center))
+    |> gen_base_with_joint conf.key_t
+    |> M.union
