45589841932798e75e9e9dcdf32dd907fbe3ab19
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-03-12 22:20:36 +0900

    pcb only

--- model/format.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd $(dirname $0)
-
-find bin -type f -name "*.ml" | xargs ocamlformat -i
-find lib -type f -name "*.ml" | xargs ocamlformat -i
-find test -type f -name "*.ml" | xargs ocamlformat -i
