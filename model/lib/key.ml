75f377440d3d56fd27234634cca92bbf8a20f3eb
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-03-12 22:16:01 +0900

    redesign

--- model/lib/key.ml
+++ model/lib/key.ml
@@ -5,6 +5,7 @@ let t = 2.2
 
 let key_bottom (w, d) (dx, dy) =
     let cube = M.cube (w, d, t) in
+    let t = t +. 0.02 in
     let center_hole = M.cylinder 2.4 t in
     let mounting_hole = M.cylinder 0.8 t in
     let pin_hole = M.cylinder 0.6 t in
@@ -14,4 +15,4 @@ let key_bottom (w, d) (dx, dy) =
         pin_hole |>> (0., -5.9, 0.);
         pin_hole |>> (5., -3.8, 0.);
     ] in
-    M.difference cube [ hole_set |>> (w /. 2. +. dx, d /. 2. +. dy, 0.) ]
+    (cube, hole_set |>> (w /. 2. +. dx, d /. 2. +. dy, -0.01))
